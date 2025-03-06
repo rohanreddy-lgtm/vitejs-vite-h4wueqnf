13b3d33265bae7254b963172714113b60495dbfe
Shankuri Rohanreddy <shankurirohan76@gmail.com>
2025-03-06 10:51:32 +0530

    changes

---
+++ src/AddBook.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const AddBook = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      onAdd({ title, author });
+      setTitle('');
+      setAuthor('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Author:</label>
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </div>
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
+
+export default AddBook;
